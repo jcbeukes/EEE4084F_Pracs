5b26691675c17781661f742c3e30339768921506
jcbeukes <36661018+jcbeukes@users.noreply.github.com>
2018-03-01 09:30:36 +0200

    balh

--- Prac0/compare.m
+++ Prac0/compare.m
@@ -1,16 +1,19 @@
 for n= [100 1000 10000 100000 1000000]
   x = rand(1,n); 
   y = rand(1,n);
+  disp(num2str(n));
+  for i = 1:5
+    tic(); 
+    mycorr(x,y); 
+    t_1 = toc();
+    disp(strcat("mycorr took: ", num2str(t_1*1000), " ms to run"));
 
-  tic(); 
-  mycorr(x,y); 
-  t_1 = toc();
-  disp(strcat("mycorr took: ", num2str(t_1*1000), " ms to run"));
-
-  tic(); 
-  corr(x,y);
-  t_2 = toc();
-  disp(strcat("corr took: ", num2str(t_2*1000), " ms to run"));
+    tic(); 
+    corr(x,y);
+    t_2 = toc();
+    disp(strcat("corr took: ", num2str(t_2*1000), " ms to run"));
+  endfor
   disp("");
+  
 endfor
 
