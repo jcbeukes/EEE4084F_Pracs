75e4d9063b066b018f4aff62e0f83181eea8729b
jcbeukes <36661018+jcbeukes@users.noreply.github.com>
2018-03-01 13:40:20 +0200

    small change

--- Prac0/compare.m
+++ Prac0/compare.m
@@ -1,18 +1,23 @@
-for n= [100 1000 10000 100000 1000000]
+for n= [100 10000 100000 1000000 100000000]
   x = rand(1,n); 
   y = rand(1,n);
   disp(num2str(n));
+  myavg = 0;
+  avg =0;
   for i = 1:5
     tic(); 
     mycorr(x,y); 
     t_1 = toc();
-    disp(strcat("mycorr took: ", num2str(t_1*1000), " ms to run"));
-
+    myavg = myavg+t_1;
+    
     tic(); 
     corr(x,y);
     t_2 = toc();
-    disp(strcat("corr took: ", num2str(t_2*1000), " ms to run"));
+    avg = avg+t_2;
+
   endfor
+  disp(strcat("mycorr took: ", num2str((myavg/4)*1000), " ms to run"));
+  disp(strcat("corr took: ", num2str((avg/4)*1000), " ms to run"));
   disp("");
   
 endfor
--- Prac0/sin_compare.m
+++ Prac0/sin_compare.m
@@ -1,10 +1,17 @@
 % Compare signals shifted in time.
-n = [1:0.1:2000];
-y = sin(n);
-y1 = y(1:100);
-y2 = y(11:110);
-subplot(2,1,1);
-plot(y1);
-subplot(2,1,2);
-plot(y2);
-corr(y1,y2)
+x = -20:0.1:20;
+outp = [];
+h = 1;
+for n = 0:0.1:2*pi
+  y1 = sin (x);
+  y2 = sin(x+n);
+  outp(h) = corr(y1, y2);
+  h = h + 1;
+endfor
+n = 0:0.1:2*pi;
+plot(n, outp);
+axis([0 2*pi -1 1]);
+xlabel('Phase shift');
+ylabel('Correlation');    
+title('Correlation between sinusoids as a function of phase shift');
+ set(gca,'fontsize',20); 
