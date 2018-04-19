267c35787d7e3f7af9dacbb65c9c87c210d4fefb
jcbeukes <36661018+jcbeukes@users.noreply.github.com>
2018-04-19 13:14:46 +0200

    Solved border issues (Johno)

--- Prac4/Prac4.cpp
+++ Prac4/Prac4.cpp
@@ -1,3 +1,27 @@
+//==============================================================================
+// Copyright (C) John-Philip Taylor
+// tyljoh010@myuct.ac.za
+//
+// This file is part of the EEE4084F Course
+//
+// This file is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>
+//
+// This is an adaptition of The "Hello World" example avaiable from
+// https://en.wikipedia.org/wiki/Message_Passing_Interface#Example_program
+//==============================================================================
+
+
 /** \mainpage Prac4 Main Page
  *
  * \section intro_sec Introduction
@@ -23,14 +47,18 @@
  *  the MPI libraries for prallel or cluster-based programming.
  */
 
+// Includes needed for the program
+
 
 #include "Prac4.h"
 
+/** This is the master node function, describing the operations
+    that the master will be doing */
 void Master(){
- int  j;
- int get[1];  //Simple single entry Return Buffer
- int send[3];    //Simple 3-entry Send Buffer (Segment Size, X Width, Y Height)
- MPI_Status stat;
+ int  j;          //! j: Loop counter
+ int get[1];      //! Simple single entry Return Buffer
+ int send[3];     //! Simple 3-entry Send Buffer (Segment Size, X Width, Y Height)
+ MPI_Status stat; //! stat: Status of the MPI application
 
  if(!Input.Read("Data/greatwall.jpg")){
   printf("Cannot read image\n");
@@ -40,38 +68,55 @@ void Master(){
  if(!Output.Allocate(Input.Width, Input.Height, Input.Components)) return;
 
  int yDiv = std::floor((float)(Input.Height)/(numprocs-1));
+ int yDiv2 = 0;
  int size = Input.Height;
- char buf [yDiv][Input.Width * Input.Components];
+ char buf [yDiv+80][Input.Width * Input.Components]; // Space for four on either side
 
- printf("0: Slave %d started\n", j);
+
  for(j = 1; j < numprocs; j++){
-  send[0] = (j < numprocs-1 ? yDiv * Input.Width * Input.Components: size * Input.Width * Input.Components);
+   tic();
+   if (j == 1){yDiv2 = yDiv+4;}
+   else if (j < numprocs-1){yDiv2 = yDiv+8;}
+   else {yDiv2 = yDiv+4;}
+  send[0] = (j < numprocs-1 ? yDiv2 * Input.Width * Input.Components: (size+4) * Input.Width * Input.Components);
   send[1] = Input.Width * Input.Components;
-  send[2] = yDiv;
+  send[2] = yDiv2;
 
   int k =0;
   int p =0;
   int col = 0;
-  for (k = yDiv*(j-1); k < yDiv*(j);k+=1){
-    for (p = 0;p < Input.Width * Input.Components;p+=1){
-        buf[col][p] = Input.Rows[k][p];
+
+
+  for (k = yDiv*(j-1)-4; k < yDiv*(j-1)+yDiv2;k+=1){
+
+    for (p = 0;p < Input.Width * Input.Components;p+=1){//<<
+        if (k<0 or k>=Input.Height){buf[col][p] = 0;}//<<
+        else{buf[col][p] = Input.Rows[k][p];}//<<
 
     }col+=1;
 
+
   }
-  printf("0: We have %d slaves\n", numprocs-1);
+ 
   MPI_Send(send, 3, MPI_INT, j, TAG, MPI_COMM_WORLD); //Send Dimensions
   MPI_Recv(get, 1, MPI_INT, j, TAG, MPI_COMM_WORLD, &stat); //Get Response - Prevents blocking
-  MPI_Send(buf, yDiv * Input.Width * Input.Components, MPI_CHAR, j, TAG, MPI_COMM_WORLD); //Send Data
+  MPI_Send(buf, yDiv2 * Input.Width * Input.Components, MPI_CHAR, j, TAG, MPI_COMM_WORLD); //Send Data
   MPI_Recv(get, 1, MPI_INT, j, TAG, MPI_COMM_WORLD, &stat); //Get Response
   size-=yDiv;
+
+  printf("0: Slave %d started\n", j);
  }
+
+
  for(j = 1; j < numprocs; j++){
-  MPI_Recv(buf, yDiv * Input.Width * Input.Components, MPI_CHAR, j, TAG, MPI_COMM_WORLD, &stat); //Get Result
+   if (j == 1){yDiv2 = yDiv+4;}
+   else if (j < numprocs-1){yDiv2 = yDiv+8;}
+   else {yDiv2 = yDiv+4;}
+  MPI_Recv(buf, yDiv2 * Input.Width * Input.Components, MPI_CHAR, j, TAG, MPI_COMM_WORLD, &stat); //Get Result
   printf("0: Slave %d Finished\n", j);
   int k =0;
   int p =0;
-  int col = 0;
+  int col = 4;
   for (k = yDiv*(j-1); k < yDiv*(j);k+=1){  //Recombine
     for (p = 0;p < Input.Width * Input.Components;p+=1){
       Output.Rows[k][p] = buf[col][p];
@@ -81,15 +126,19 @@ void Master(){
 
 
   printf("0: Slave %d Reassembled\n", j);
+  printf("Time = %lg ms\n", (double)toc()/1e-3);
  }
 
- if(!Output.Write("Data/Output.jpg")){
+ if(!Output.Write("Data/Output4.jpg")){
   printf("Cannot write image\n");
   return;
  }
+  //! <h3>Output</h3> The file Output.jpg will be created on success to save
+ //! the processed output.
 }
 //------------------------------------------------------------------------------
 
+/** This is the Slave function, the workers of this MPI application. */
 void Slave(int ID){
  int  dim[3];
  char idstr[32];
@@ -137,10 +186,11 @@ void Slave(int ID){
 
 
  MPI_Send(Out, dim[0], MPI_CHAR, 0, TAG, MPI_COMM_WORLD);
- // End of "Hello World" example................................................
+
 }
 //------------------------------------------------------------------------------
 
+/** This is the entry point to the program. */
 int main(int argc, char** argv){
  int myid;
 
